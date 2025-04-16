0b8b8160afad4f72ec6710088d7e283780afb0cd
Yatharth Bisht <133650394+yatharth230703@users.noreply.github.com>
2025-04-16 07:23:00 +0530

    Update flashcards.py

--- semi_final_pipelines/flashcards.py
+++ semi_final_pipelines/flashcards.py
@@ -68,7 +68,7 @@ def generate_mindmap(text,subject_name):
 
 def flashcard(text_content,subject_name , out):
     client = genai.Client(
-        api_key="AIzaSyC1C1qYKIDY4ber5GT7S7p5-HqLRUmLbs8",
+        api_key="",
     )
 
     model = "gemini-2.0-flash"
